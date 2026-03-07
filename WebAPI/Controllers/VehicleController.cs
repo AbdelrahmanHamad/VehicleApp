9858a4f1e578ea8f751432bfdf3ef6e7eb0554b5
Abdelrahman Hamad <abdelrahman.hamad2003@gmail.com>
2026-03-07 14:21:35 +0300

    feat(backend): implement vehicle controller endpoints

---
+++ WebAPI/Controllers/VehicleController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IVehicleService _vehicleService;
+        public VehicleController(IVehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
+        [HttpGet("makes")]
+        public async Task<ActionResult<List<VehicleMakeDto>>> GetAllMakes()
+        {
+            var makes = await _vehicleService.GetAllMakesAsync();
+            return Ok(makes);
+        }
+
+        [HttpGet("types/{makeId}")]
+        public async Task<ActionResult<List<VehicleTypeDto>>> GetTypes(int makeId)
+        {
+            var types = await _vehicleService.GetVehicleTypesAsync(makeId);
+            if (types == null || !types.Any())
+            {
+                return NotFound($"No vehicle types found for Make ID {makeId}");
+            }
+            return Ok(types);
+        }
+
+        [HttpGet("models/{makeId}/{year}")]
+        public async Task<ActionResult<List<VehicleModelDto>>> GetModels(int makeId, int year)
+        {
+            var models = await _vehicleService.GetModelsAsync(makeId, year);
+            return Ok(models);
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -12,15 +12,17 @@ builder.Services.AddHttpClient<IVehicleService, VehicleService>(client =>
 
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
